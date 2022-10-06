f954b098c77e6ba3c1070560353637961f6f094c
AY2018 <100161456+AY2018@users.noreply.github.com>
2022-10-06 21:35:22 +0200

    FINISHED PHP

--- form.php
+++ form.php
@@ -3,13 +3,14 @@
   <title>réponse du serveur</title>
   </head>
   <body>
-  <?php print "bonjour monsieur ";
+  <?php print "Thank you for the message ";
   print $_POST["name"] ;
-  print " ";
+  print "! <BR><BR>I will respond to you as soon as I can to the following email address: ";
   print $_POST["email"];
-  print "<BR><BR><HR>";
+  print "<BR><BR>";
+  print 'And I am pleased to confirm that the following message has been succesfully sent : "';
   print $_POST["message"];
-	print " Your message has been sent";
+  print '".';
   ?>
   </body>
-</html> 
+</html>
