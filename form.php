ea555d2ab1b6efa2b9976bd02c7ab00892d1ebd3
AY2018 <100161456+AY2018@users.noreply.github.com>
2022-10-06 15:42:59 +0200

    change php

---
+++ form.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+  <title>réponse du serveur</title>
+  </head>
+  <body>
+  <?php print "bonjour monsieur ";
+  print $_POST["name"] ;
+  print " ";
+  print $_POST["email"];
+  print "<BR><BR><HR>";
+  print $_POST["message"];
+	print " Your message has been sent successfully,";
+  ?>
+  </body>
+</html> 
